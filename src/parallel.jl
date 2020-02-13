990c77a6cbc4a18579eb66d35181e930b4c224f1
MarcMush <marc.ittel@gmail.com>
2020-02-13 14:46:28 +0100

    fix init-parallel

--- src/parallel.jl
+++ src/parallel.jl
@@ -1,13 +1,13 @@
 
-function init_parallel(N::Integer = Sys.CPU_THREADS; kw...)
-    if N < 1
-        error("Number of workers must be greater than 0")
-    elseif N == 1 && workers() != [1]
+function init_parallel(n::Integer = Sys.CPU_THREADS; kw...)
+    if n < 1
+        error("number of workers must be greater than 0")
+    elseif n == 1 && workers() != [1]
         rmprocs(workers())
-    elseif N > nworkers()
-        p = addprocs(N - (nprocs() == 1 ? 0 : nworkers()); kw...)
-    elseif N < nworkers()
-        rmprocs(workers()[N + 1:end])
+    elseif n > nworkers()
+        p = addprocs(n - (nprocs() == 1 ? 0 : nworkers()); kw...)
+    elseif n < nworkers()
+        rmprocs(workers()[n + 1:end])
     end
     @everywhere @eval using EMSx
     return workers()
