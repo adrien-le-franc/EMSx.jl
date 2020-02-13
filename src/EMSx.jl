a18e4b50e71d775d29eb0ddc4f8e788c8020c147
MarcMush <marc.ittel@gmail.com>
2020-02-13 11:46:07 +0100

    add DIR constant

--- src/EMSx.jl
+++ src/EMSx.jl
@@ -6,6 +6,8 @@ using Dates, TimeZones, CSV, DataFrames, CodecZlib
 using JLD
 using HTTP
 
+const DIR = dirname(@__DIR__)
+
 include("parallel_progress.jl")
 include("struct.jl")
 include("database_interface.jl")
